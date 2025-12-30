6b8c2fb44986a974fc4b8bbb997978026b378608
t0mbo192 <tomlett192@gmail.com>
2025-12-29 21:42:10 -0500

    added formatting to the main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import read_book_content
 from stats import count_characters
+import sys
 
 def main():
 
@@ -7,10 +8,16 @@ def main():
 
     number_of_words_in_book = read_book_content(book)
     number_of_characters_in_book = count_characters(book)
-
+    print("========== BOOKBOT ===========")
+    print(f"Analyzing book found at {book}")
+    print("---------- Word Count ----------")
     print(f"Found {number_of_words_in_book} total words")
+    print("-------- Character Count -------")
     for key, value in number_of_characters_in_book.items():
         print(f"{key}: {value}")
+    print("========== END ==========")
+
+    print(sys.argv[1])
 
 if __name__ == '__main__':
     main()
